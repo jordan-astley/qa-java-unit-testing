f8501ee9f1e8aa44363e0522a2ada90590b4e85f
AnaCamarasan <ana.camarasan@gmail.com>
2024-10-30 11:58:12 +0000

    implemented fizzbuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,13 @@
 public class FizzBuzz {
-//    public int runFizzBuzz (int number) {
-//        if (number % 15 == 0) {
-//            return
-//        }
-//    }
+    public String runFizzBuzz (int number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return Integer.toString(number);
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,11 @@ public class Main {
 
         System.out.println(calc.add(3,4));
         System.out.println(calc.divide(10,0));
+
+        FizzBuzz fizz = new FizzBuzz();
+        System.out.println(fizz.runFizzBuzz(5));
+        System.out.println(fizz.runFizzBuzz(15));
+        System.out.println(fizz.runFizzBuzz(3));
+        System.out.println(fizz.runFizzBuzz(2));
     }
 }
