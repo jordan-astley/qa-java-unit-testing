340622404d0504df99f167dccef5a4a911addea7
jtma <jtm.astley@outlook.com>
2024-10-30 11:36:22 +0000

    implemented calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,5 +3,16 @@ public class Calculator {
 
     public int add(int x, int y) {return x + y;}
 
+    public int subtract(int x, int y) {return x - y;}
+
+    public int multiply(int x, int y) {return x * y;}
+
+    public int divide(int x, int y) {
+        if (y == 0) {
+            return null;
+        } else {
+            return x / y;
+        }
+    }
 
 }
